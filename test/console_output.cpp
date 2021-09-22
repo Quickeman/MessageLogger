9427c624cf31b0d70aaddbc64eb34c256546f940
Quickeman <finnquicke@hotmail.com>
2021-09-22 13:09:33 +0100

    Added proper fail conditions to unit tests

--- test/console_output.cpp
+++ test/console_output.cpp
@@ -10,11 +10,17 @@ int main() {
     logger.config_cout(true);
     logger.config_textFile(false, "testlog.txt");
 
+    test::check(cout.good(), "Before logging, cout not good.");
+
     // Log some messages
     logger.info("This is some console information.");
     logger.warn("This is a console warning.");
     logger.error("This is a console error message.");
 
+    test::check(cout.good(), "After logging, pre-output, cout not good.");
+
     // Wait so the messages have time to get printed
     this_thread::sleep_for(chrono::milliseconds(20));
+
+    test::check(cout.good(), "After output, cout not good.");
 }
--- test/log_file_output.cpp
+++ test/log_file_output.cpp
@@ -7,10 +7,11 @@ using namespace std;
 
 int main() {
     const string logFileName = "testlog1.txt";
-    
+
     // Clear the log file to avoid confusion
     fstream lf;
     lf.open(logFileName, ios::out | ios::trunc);
+    test::check(lf.good(), "File clearing stream not good.");
     lf.close();
 
     // Configure logger
@@ -24,4 +25,17 @@ int main() {
 
     // Wait so the messages have time to get printed
     this_thread::sleep_for(chrono::milliseconds(20));
+
+    string fileContents;
+    fileContents.resize(200);
+    lf.open(logFileName, ios::in);
+    test::check(lf.good(), "Retrieving file contents, fstream not good.");
+    lf.read(const_cast<char*>(fileContents.data()), 200);
+    test::check(!fileContents.empty(), "Log file empty.");
+    lf.close();
+    int nlCount = 0;
+    for (auto& c : fileContents)
+        if (c == '\n')
+            nlCount++;
+    test::check(nlCount, 3, 0, "Log file does not contain the right number of new line characters.");
 }
